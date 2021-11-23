d8d016dd4771036d76adbf88e725ac402e723088
riadh2 <33968382+riadh2@users.noreply.github.com>
2021-11-23 16:43:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,43 @@
-//Create our express and socket.io servers
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const {v4: uuidV4} = require('uuid')
+const express = require("express");
+const app = express();  
+const server = require("http").Server(app);
 
-app.set('view engine', 'ejs') // Tell Express we are using EJS
-app.use(express.static('public')) // Tell express to pull the client script from the public folder
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
-// If they join the base link, generate a random UUID and send them to a new room with said UUID
-app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`)
-})
-// If they join a specific room, then render that room
-app.get('/:room', (req, res) => {
-    res.render('room', {roomId: req.params.room})
-})
-// When someone connects to the server
-io.on('connection', socket => {
-    // When someone attempts to join the room
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)  // Join the room
-        socket.broadcast.emit('user-connected', userId) // Tell everyone else in the room that we joined
-        
-        // Communicate the disconnection
-        socket.on('disconnect', () => {
-            socket.broadcast.emit('user-disconnected', userId)
-        })
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on('disconnect',()=>{
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+
+
     })
-})
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+});
+
 server.listen(process.env.PORT || 3030);
