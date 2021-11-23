445130498debc51d4343a86d86e751f05d168915
riadh2 <33968382+riadh2@users.noreply.github.com>
2021-11-23 15:23:51 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -100,8 +100,6 @@ function saveFile(recordedChunks){
   document.body.removeChild(downloadLink);
 }
 var peer = new Peer('undefined',{
-      host: 'http://3.139.70.133',
-      port: 9000,
       path: '/myapp'
     });
 let myVideoStream;
