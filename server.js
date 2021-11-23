3de313e19dc2c94850f88bafdd213552f0c2df3a
riadh2 <33968382+riadh2@users.noreply.github.com>
2021-11-23 12:23:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const app = express();
+const app = express();  
 const server = require("http").Server(app);
+
 const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
@@ -31,7 +32,6 @@ io.on("connection", (socket) => {
     socket.on('disconnect',()=>{
       socket.to(roomId).broadcast.emit("user-disconnected", userId);
 
-
     })
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message, userName);
@@ -40,3 +40,4 @@ io.on("connection", (socket) => {
 });
 
 server.listen(process.env.PORT || 3030);
+
